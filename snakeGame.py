230d48e02b1c05ea69e56368bea1aaefee02f5fb
mhreid12 <72514872+mhreid12@users.noreply.github.com>
2021-11-18 10:06:06 -0500

    Update randColor
    Updates randColor so game over message doesn't have same color as background

--- snakeGame.py
+++ snakeGame.py
@@ -126,6 +126,7 @@ while True:
         foodPos = [random.randrange(1,72)*10,random.randrange(1,46)*10]
     foodSpawn = True
     playSurface.fill(randColor)
+    randColor=pygame.Color(random.randrange(255),random.randrange(255),random.randrange(255))
     for pos in snakeBody:
         pygame.draw.rect(playSurface, yellow, pygame.Rect(pos[0],pos[1],10,10))
     pygame.draw.rect(playSurface,brown,pygame.Rect(foodPos[0],foodPos[1],10,10))
